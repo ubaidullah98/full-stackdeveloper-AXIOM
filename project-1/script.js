de42ced1087c324e454635cfa754e4e8f9f467c6
ubaidullah98 <57267154+ubaidullah98@users.noreply.github.com>
2021-05-04 05:55:31 +0500

    updated JS
    code updated of  javascript

--- project-1/script.js
+++ project-1/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const password2 = document.getElementById('confirm password');
 
 function showError(input,message){
     const formControl = input.parentElement;
@@ -16,53 +16,35 @@ function showSuccess(input){
     const formControl = input.parentElement;    
     formControl.className = 'form-control success';
     const small = formControl.querySelector('small');
+}
+
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function checkRequired(inputArray){
+    inputArray.forEach(function(input) {
+        if(input.value === '')
+        showError(input, `${getFieldId(input)} is required`);
+         
+        else{
+            showSuccess(input);
+        }
+        
+
+    });
+}
+
+function getFieldId(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+
 
 }
 
-
-
-
-
-
-
-
 form.addEventListener('submit', function(e){
     e.preventDefault();
-   
-    if(username.value === ''){
 
-       showError(username, 'username is required');
-    }
-    else{
-        showSuccess(username);
-    }
-
-
-    if(email.value === ''){
-
-        showError(email, 'email is required');
-     }
-     else{
-         showSuccess(email);
-     }
-
-     if(password.value === ''){
-
-        showError(password, 'password is required');
-     }
-     else{
-         showSuccess(password);
-     }
-
-     if(password2.value === ''){
-
-        showError(password2, 'confirm password is required');
-     }
-     else{
-         showSuccess(password2);
-     }
-
-
-
-
+    checkRequired([username,email,password,password2])
+    
 });
