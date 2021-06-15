5a6e639aba21b95970a1b1db0fa06f78fb135746
ubaidullah98 <57267154+ubaidullah98@users.noreply.github.com>
2021-06-15 13:07:20 +0500

    project5 HTML,CSS,JS

---
+++ project-5/script.js
@@ -0,0 +1,16 @@
+const menuToggle = document.getElementById('toggle');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.getElementById('modal');
+
+menuToggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => modal.classList.add('show-modal') );
+
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+window.addEventListener('click', e =>
+    e.target === modal ? modal.classList.remove('show-modal') : false
+);
